666d9b1253f5fa3778b2191bdafff559b83ef7e7
test samp <testsamp0606@gmail.com>
2026-01-05 19:30:13 +0000

    make all dashboard links to dashboard only remove hr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,6 @@ export default function Home() {
       <Button asChild>
         <Link href="/dashboard">Super Admin Dashboard</Link>
       </Button>
-      <Button asChild>
-        <Link href="/hr/dashboard">HR Dashboard</Link>
-      </Button>
       <Button asChild variant="outline">
         <Link href="/employee/dashboard">Employee Dashboard</Link>
       </Button>
