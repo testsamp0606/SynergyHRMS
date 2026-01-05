33345c5ca56da9a411d4602f857d2729282a998a
test samp <testsamp0606@gmail.com>
2026-01-05 19:39:23 +0000

    remove all hr related link content as blank or sample hi message ,I will

--- src/app/hr/recruitment/page.tsx
+++ src/app/hr/recruitment/page.tsx
@@ -1,13 +1,3 @@
-import { Header } from "@/components/layout/header"
-import { RecruitmentForm } from "@/app/dashboard/recruitment/recruitment-form"
-
-export default function RecruitmentPage() {
-  return (
-    <div className="flex min-h-screen w-full flex-col">
-      <Header title="Recruitment Management" />
-      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-        <RecruitmentForm />
-      </main>
-    </div>
-  )
+export default function Page() {
+  return <div>Hi</div>
 }
