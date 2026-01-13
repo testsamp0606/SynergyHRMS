9a0f48506376321fac65bac35d4e83b362168a98
test samp <testsamp0606@gmail.com>
2026-01-13 04:12:58 +0000

    Make this calender as a matrix table (_for element <CardContent>_)

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -32,14 +32,14 @@ function Calendar({
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
-        head_row: "flex",
+        head_row: "grid grid-cols-7",
         head_cell:
           "text-muted-foreground rounded-md w-full font-normal text-[0.8rem]",
-        row: "flex w-full mt-2",
-        cell: "h-16 w-full text-center text-sm p-0 relative focus-within:relative focus-within:z-20",
+        row: "grid grid-cols-7 w-full mt-2",
+        cell: "h-20 w-full text-center text-sm p-0 relative focus-within:relative focus-within:z-20 border",
         day: cn(
           buttonVariants({ variant: "ghost" }),
-          "h-16 w-full p-0 font-normal aria-selected:opacity-100"
+          "h-full w-full p-0 font-normal aria-selected:opacity-100"
         ),
         day_selected:
           "bg-accent text-accent-foreground",
